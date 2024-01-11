d3aaa2150ecc80c63bab9f66b3dc00242b7b8340
Juan Jumbo <tiniyan24@gmail.com>
2024-01-10 20:03:42 -0500

    Taller21

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Pregunta />
+</template>
+
+<script>
+import Pregunta from './components/Pregunta.vue'
+//import PaginaPokemon from './components/PaginaPokemon.vue'
+export default {
+  name: 'App',
+  components: {
+    Pregunta,
+    //PaginaPokemon
+  },
+  methods: {
+    calcularValor() {
+      return 5 + 5 * 10;
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
